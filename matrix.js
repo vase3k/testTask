00d45e1f4b52f4eff38a1789818c158d0c6e89d1
Vasiliy VV <vase3k@gmail.com>
2024-04-07 12:58:48 +0300

    aaa

--- matrix.js
+++ matrix.js
@@ -1,8 +1,9 @@
-const w = +prompt('width', 6);
-const h = +prompt('height', 6);
+//const w = +prompt('width', 6);
+//const h = +prompt('height', 6);
+const w = 6, h = 5;
 
 function matrix(w, h) {
-    let result = new Array(h).fill().map(() => new Array(w).fill(""));
+    let result = new Array(h).fill().map(() => new Array(w).fill(''));
     let counter = 1;
     let startCol = 0;
     let endCol = w - 1;
@@ -38,4 +39,4 @@ function matrix(w, h) {
     return result;
 }
 
-console.log(matrix(w,h));
+console.table(matrix(w,h));
